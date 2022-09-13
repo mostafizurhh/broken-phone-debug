51fa79eddca9b397df34db979b8aa666e29028a4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-13 06:14:45 +0100

    new files uploaded

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const loadPhones = async(searchText, dataLimit) =>{
 
 const displayPhones = (phones, dataLimit) =>{
     const phonesContainer = document.getElementById('phones-container');
-    phonesContainer.textContent = '';
+    // phonesContainer.textContent = '';
     // display 10 phones only 
     const showAll = document.getElementById('show-all');
     if(dataLimit && phones.length > 10) {
@@ -15,7 +15,7 @@ const displayPhones = (phones, dataLimit) =>{
         showAll.classList.remove('d-none');
     }
     else{
-        showAll.classList.add('d-none');
+        showAll.classList.add('d-hidden');
     }
     
 
@@ -31,9 +31,9 @@ const displayPhones = (phones, dataLimit) =>{
     phones.forEach(phone =>{
         const phoneDiv  = document.createElement('div');
         phoneDiv.classList.add('col');
-        phoneDiv.innerHTML = `
+        phonesContainer.innerHTML = `
         <div class="card p-4">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+            <img src="${phone.images}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -56,21 +56,21 @@ const processSearch = (dataLimit) =>{
 }
 
 // handle search button click
-document.getElementById('btn-search').addEventListener('click', function(){
+document.getElementById('#btn-search').addEventListener('click', function(){
     // start loader
     processSearch(10);
 })
 
 // search input field enter key handler
 document.getElementById('search-field').addEventListener('keypress', function (e) {
-    if (e.key === 'Enter') {
+    if (e.key === 'enter') {
         processSearch(10);
     }
 });
 
 const toggleSpinner = isLoading => {
     const loaderSection = document.getElementById('loader');
-    if(isLoading){
+    if(!isLoading){
         loaderSection.classList.remove('d-none')
     }
     else{
@@ -85,7 +85,7 @@ document.getElementById('btn-show-all').addEventListener('click', function(){
 })
 
 const loadPhoneDetails = async id =>{
-    const url =`https://openapi.programming-hero.com/api/phone/${id}`;
+    const url =`www.openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhoneDetails(data.data);
@@ -98,8 +98,8 @@ const displayPhoneDetails = phone =>{
     const phoneDetails = document.getElementById('phone-details');
     console.log(phone.mainFeatures.sensors[0]);
     phoneDetails.innerHTML = `
-        <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'No Release Date Found'}</p>
-        <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage : 'No Storage Information '}</p>
+        <p>Release Date: ${phone.releaseDate}</p>
+        <p>Storage: ${phone.mainFeatures}</p>
         <p>Others: ${phone.others ? phone.others.Bluetooth : 'No Bluetooth Information'}</p>
         <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     `
